ac275c06c9b6311b79344ace7d13c5eb86781c92
Munzer221 <altcoinkid@gmail.com>
2026-01-02 00:30:12 -0500

    auto-save 2026-01-02_05-30-11

--- lib/screens/export_shifts_screen.dart
+++ lib/screens/export_shifts_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import '../models/shift.dart';
 import '../models/job.dart';
 import '../providers/shift_provider.dart';
