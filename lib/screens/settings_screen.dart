2c8148ad05d6bd38742aaabdcd65002661d11ad6
Munzer221 <altcoinkid@gmail.com>
2026-01-02 20:17:53 -0500

    auto-save 2026-01-03_01-17-53

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/foundation.dart' show kIsWeb;
+import 'package:flutter/foundation.dart' show kIsWeb, kDebugMode;
 import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
